ec34204bb27d4b582bab1a7988bba7199db9bdbb
lundeguten98 <49440779+lundeguten98@users.noreply.github.com>
2019-04-11 14:50:28 +0200

    Update data.js
    fått ut kommune navn

--- scripts/data.js
+++ scripts/data.js
@@ -1,20 +1,42 @@
-
 var befolkning = "http://wildboy.uib.no/~tpe056/folk/104857.json"
 var sysselsatt = "http://wildboy.uib.no/~tpe056/folk/100145.json"
 var utdanning = "http://wildboy.uib.no/~tpe056/folk/85432.json"
 
 
 function visData(data) {
-  var res = document.getElementById("befolkning");
-  for (item in data) {
-    for (elem in item) {
-      res.value = res.value + data[item];
-    }
+  var oversikt = document.getElementById("Overview");
+  oversikt.innerHTML = "";
+
+  var list = document.createElement("ol");
+  var list2 = document.createElement("ol");
+  for (item in data.elementer) {
+    var info = item;//data.elementer[item].kommunenummer
+    var lisElt = document.createElement("li");
+    var lisElt2 = document.createElement("li");
+    lisElt.innerHTML = info;
+    lisElt2.innerHTML = data.elementer[item].kommunenummer;
+    list.appendChild(lisElt);
+    list2.appendChild(lisElt2);
+
+
+
+
+    //res.value = res.value + data[item].eierskap;
+    //console.log(item);
+    console.log(data.elementer[item].kommunenummer);
 
 
 
   }
+  oversikt.appendChild(list);
 }
+
+
+
+function visDetails(text) {
+
+}
+
 
 function getData() {
   var xhr = new XMLHttpRequest();
