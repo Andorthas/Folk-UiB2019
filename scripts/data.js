b8b069dea63aeeb6bb16582cb32011ddef14b778
lundeguten98 <49440779+lundeguten98@users.noreply.github.com>
2019-04-12 16:12:31 +0200

    Update data.js
    Nyeste endringer, henter ut kommune, kommune nr og antall kvinner i kommunene, fekk ikkje til å hente ut mann foreløpig.

--- scripts/data.js
+++ scripts/data.js
@@ -9,26 +9,48 @@ function visData(data) {
 
   var list = document.createElement("ol");
   var list2 = document.createElement("ol");
+  var list3 = document.createElement("ol");
   for (item in data.elementer) {
     var info = item;//data.elementer[item].kommunenummer
+    var info2 = data.elementer[item].kommunenummer;
+    var info3 = data.elementer[item];
     var lisElt = document.createElement("li");
     var lisElt2 = document.createElement("li");
+    var lisElt3 = document.createElement("li");
     lisElt.innerHTML = info;
-    lisElt2.innerHTML = data.elementer[item].kommunenummer;
+    lisElt2.innerHTML = info2;
+    lisElt3.innerHTML = verdi;
     list.appendChild(lisElt);
     list2.appendChild(lisElt2);
+    list3.appendChild(lisElt3);
+    //console.log(verdi);
+
+
+    for (c in info3) {
+      var måling = info3[c];
+      //console.log(måling);
+      for (kvinne in måling) {
+        var verdi = måling[kvinne];
+      }
+    }
+
+
+
+
 
 
 
 
     //res.value = res.value + data[item].eierskap;
     //console.log(item);
-    console.log(data.elementer[item].kommunenummer);
+    //console.log(data.elementer[item].kommunenummer);
 
 
 
   }
   oversikt.appendChild(list);
+  oversikt.appendChild(list2);
+  oversikt.appendChild(list3);
 }
 
 
