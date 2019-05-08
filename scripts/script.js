843ec29cb720c1fd8ab0adace8a894d30b990b72
Audun Andre Naley <audunandrenaley@hotmail.com>
2019-05-08 19:49:47 +0200

    update
    Fikset detaljer og sammenligning + mer responsivitet. Nesten alt er gjort nå.

--- scripts/script.js
+++ scripts/script.js
@@ -19,16 +19,16 @@ function changeTab(event, tabName) {
 function Grensesnitt(url, onload) {
 	if (this instanceof Grensesnitt) {
 		if (url) {
-			if (typeof url == "string") {
+			if (typeof url === "string") {
 				this.url = url;
-				
-				this.load = function(){
+
+				this.load = function () {
 					var request = new XMLHttpRequest();
 					request.open("GET", this.url, false);
 					request.send(null);
-					if (request.status == 200 && request.readyState == 4) {
+					if (request.status === 200 && request.readyState === 4) {
 						this.data = JSON.parse(request.responseText);
-						if(onload) {
+						if (onload) {
 							onload();
 						}
 					} else {
@@ -36,8 +36,8 @@ function Grensesnitt(url, onload) {
 					}
 				}
 
-				this.getNames = function() {
-					if(this.data){
+				this.getNames = function () {
+					if (this.data) {
 						var places = this.data.elementer;
 						var names = [];
 						for (var place in places) {
@@ -46,13 +46,13 @@ function Grensesnitt(url, onload) {
 							}
 						}
 						return names;
-					}else{
+					} else {
 						throw "Ingen data, prøv load(); først";
 					}
 				}
 
-				this.getIDs = function() {
-					if(this.data){
+				this.getIDs = function () {
+					if (this.data) {
 						var places = this.data.elementer;
 						var ids = [];
 						for (var place in places) {
@@ -61,29 +61,29 @@ function Grensesnitt(url, onload) {
 							}
 						}
 						return ids;
-					}else{
+					} else {
 						throw "Ingen data, prøv load(); først";
 					}
 				}
 
-				this.getInfo = function(index) {
-					if(this.data){
+				this.getInfo = function (index) {
+					if (this.data) {
 						index = ("0000" + index.toString()).slice(-4);
 						var places = this.data.elementer;
 						for (var place in places) {
 							if (places.hasOwnProperty(place)) {
 								if (places[place].kommunenummer == index) {
-									return {"name": place, "id": index, "data": places[place]};
+									return { "name": place, "id": index, "data": places[place] };
 								}
 							}
 						}
 						return null;
-					}else{
+					} else {
 						throw "Ingen data, prøv load(); først";
 					}
 				}
 
-				this.beggeKjønn = function() {
+				this.beggeKjonn = function () {
 					var places = this.data.elementer;
 					var beggeKjønn = [];
 					for (var place in places) {
@@ -110,20 +110,22 @@ var befolkning_g;
 var sysselsetting_g;
 var utdanning_g;
 
-window.onload = function() {
-	befolkning_g = new Grensesnitt("http://wildboy.uib.no/~tpe056/folk/104857.json", function() {sysselsetting_g.load()});
-	sysselsetting_g = new Grensesnitt("http://wildboy.uib.no/~tpe056/folk/100145.json", function() {utdanning_g.load()});
-  utdanning_g = new Grensesnitt("http://wildboy.uib.no/~tpe056/folk/85432.json", function() {} );
-  
+// var sysselsettingData = [];
+
+window.onload = function () {
+	befolkning_g = new Grensesnitt("http://wildboy.uib.no/~tpe056/folk/104857.json", function () { sysselsetting_g.load() });
+	sysselsetting_g = new Grensesnitt("http://wildboy.uib.no/~tpe056/folk/100145.json", function () { utdanning_g.load() });
+	utdanning_g = new Grensesnitt("http://wildboy.uib.no/~tpe056/folk/85432.json", function () { });
+
 	befolkning_g.load();
 	sysselsetting_g.load();
 	utdanning_g.load();
 
 	var names = befolkning_g.getNames();
 	var ids = befolkning_g.getIDs();
-	var beggeKjønn = befolkning_g.beggeKjønn();
+	var beggeKjonn = befolkning_g.beggeKjonn();
 
-  var tab = document.getElementById("table");
+	var tab = document.getElementById("table");
 
 	for (var i = 0; i < names.length; ++i) {
 
@@ -135,55 +137,158 @@ window.onload = function() {
 
 		cell0.innerText = names[i];
 		cell1.innerText = ids[i];
-		cell2.innerText = beggeKjønn[i];
+		cell2.innerText = beggeKjonn[i];
 	}
 }
 
 /*---------------- SEARCH-FUNCTION --------------- */
 
-function search() {
-	var id = document.getElementById("searchInput").value;
-	if (!id) {
+function search(kommuneId, tableId, compareId) {
+	if (!kommuneId || !tableId) {
 		return;
 	}
-  var data = befolkning_g.getInfo(id);
-	 //var result1 = sysselsetting_g.getInfo(id);
-	 //var result2 = utdanning_g.getInfo(id);
 
-	if(data){
-		document.getElementById("searchMessage").innerText = "";
-		var table = document.getElementById("searchTable");
-		
-		var newTbody = document.createElement("tbody");
-		table.tBodies[0].parentNode.replaceChild(newTbody, table.tBodies[0]);
+	var befolkningsdata = befolkning_g.getInfo(kommuneId);
+	var sysselsettingsdata = sysselsetting_g.getInfo(kommuneId);
+	var sysselsettingsdataForCompare;
+	var utdanningsdata = utdanning_g.getInfo(kommuneId);
 
-		
-		var menn = data.data["Menn"];
-		var kvinner = data.data["Kvinner"];
+	if (compareId) {
+		sysselsettingsdataForCompare = sysselsetting_g.getInfo(compareId);
+	}
 
-		for(var year in menn){
-			if(menn.hasOwnProperty(year)){
-				
-				var row = table.tBodies[0].insertRow(0);
-				var nameCell = row.insertCell(0);
-				nameCell.innerText = data.name;
-				var idCell = row.insertCell(1);
-				idCell.innerText = data.id;
+	if (!befolkningsdata) {
+		document.getElementById("searchMessage").innerText = "Data på kommunenummer " + kommuneId + " finnes ikke.";
+		document.getElementById(tableId).style.display = "none";
+		return;
+	}
 
-				var yearCell = row.insertCell(-1);
-				yearCell.innerText = year;
+	document.getElementById("searchMessage").innerText = "";
+	var table = document.getElementById(tableId);
+
+	var newTbody = document.createElement("tbody");
+	table.tBodies[0].parentNode.replaceChild(newTbody, table.tBodies[0]);
+
+
+	var menn = befolkningsdata.data.Menn;
+	var kvinner = befolkningsdata.data.Kvinner;
+
+	var mennSysselsetting = sysselsettingsdata.data.Menn;
+	var kvinnerSysselsetting = sysselsettingsdata.data.Kvinner;
+
+	var mennUtdanningKort = utdanningsdata.data["03a"].Menn;
+	var kvinnerUtdanningKort = utdanningsdata.data["03a"].Kvinner;
+	var mennUtdanningLang = utdanningsdata.data["04a"].Menn;
+	var kvinnerUtdanningLang = utdanningsdata.data["04a"].Kvinner;
+
+	for (var year in menn) {
+		var lastYear = year - 1;
+
+		if (menn.hasOwnProperty(year)) {
+
+			var row = table.tBodies[0].insertRow(0);
+
+			/*---------------- KOMMUNENAVN --------------- */
+
+			var nameCell = row.insertCell(0);
+			nameCell.innerText = befolkningsdata.name;
+
+			/*---------------- KOMMUNENUMMER --------------- */
+
+			var idCell = row.insertCell(1);
+			idCell.innerText = befolkningsdata.id;
+
+			/*---------------- ÅRSTALL --------------- */
+
+			var yearCell = row.insertCell(-1);
+			yearCell.innerText = year;
+
+			/*---------------- MENN OG KVINNER --------------- */
+
+			if (!compareId) {
 				var mennCell = row.insertCell(-1);
 				mennCell.innerText = menn[year];
 				var kvinCell = row.insertCell(-1);
 				kvinCell.innerText = kvinner[year];
 			}
+
+			/*---------------- SYSSELSETTING --------------- */
+
+			var syssMennCell = row.insertCell(-1);
+			syssMennCell.innerText = mennSysselsetting[year]
+			var syssKvinnerCell = row.insertCell(-1);
+			syssKvinnerCell.innerText = kvinnerSysselsetting[year]
+
+			/*---------------- UTDANNING --------------- */
+
+			if (!compareId) {
+				var utdMennKortCell = row.insertCell(-1);
+				utdMennKortCell.innerText = formatCellData(mennUtdanningKort[year]);
+				var utdKvinnerKortCell = row.insertCell(-1);
+				utdKvinnerKortCell.innerText = formatCellData(kvinnerUtdanningKort[year]);
+				var utdMennLangCell = row.insertCell(-1);
+				utdMennLangCell.innerText = formatCellData(mennUtdanningLang[year]);
+				var utdKvinnerLangCell = row.insertCell(-1);
+				utdKvinnerLangCell.innerText = formatCellData(kvinnerUtdanningLang[year]);
+			}
+			if (compareId) {
+				/*---------------- VEKST --------------- */
+
+				var mennVekstKommune1 = calculateGrowth(mennSysselsetting[year], mennSysselsetting[lastYear]).toFixed(2);
+				var kvinnerVekstKommune1 = calculateGrowth(kvinnerSysselsetting[year], kvinnerSysselsetting[lastYear]).toFixed(2);
+
+				var mennSysselsettingKommune2 = sysselsettingsdataForCompare.data.Menn;
+				var kvinnerSysselsettingKommune2 = sysselsettingsdataForCompare.data.Kvinner;
+
+				var mennVekstKommune2 = calculateGrowth(mennSysselsettingKommune2[year], mennSysselsettingKommune2[lastYear]).toFixed(2);
+				var kvinnerVekstKommune2 = calculateGrowth(kvinnerSysselsettingKommune2[year], kvinnerSysselsettingKommune2[lastYear]).toFixed(2);
+
+				var vekstMennCell = row.insertCell(-1);
+				vekstMennCell.innerText = mennVekstKommune1 + "%";
+
+				if (mennVekstKommune1 >= mennVekstKommune2) {
+					vekstMennCell.classList.add('highest-growth');
+				}
+
+				var vekstKvinnerCell = row.insertCell(-1);
+				vekstKvinnerCell.innerText = kvinnerVekstKommune1 + "%";
+
+				if (kvinnerVekstKommune1 >= kvinnerVekstKommune2) {
+					vekstKvinnerCell.classList.add('highest-growth');
+				}
+			}
+
 		}
 
-    table.style.display = "table";
-    
-	}else{
-		document.getElementById("searchMessage").innerText = "Data på kommunenummer " + id + " finnes ikke.";
-		document.getElementById("searchTable").style.display = "none";
-	}
+		table.style.display = "table";
 
+
+	}
 }
+/*---------------- COMPARE-FUNCTION --------------- */
+
+function compare(searchInputId1, tableId1, searchInputId2, tableId2) {
+	var kommuneId1 = document.getElementById(searchInputId1).value;
+	var kommuneId2 = document.getElementById(searchInputId2).value;
+
+	search(kommuneId1, tableId1, kommuneId2);
+	search(kommuneId2, tableId2, kommuneId1);
+}
+
+/*---------------- CALCULATE GROWTH-FUNCTION --------------- */
+
+function calculateGrowth(thisYear, lastYear) {
+	return (1 - (lastYear / thisYear)) * 100;
+}
+
+
+/*---------------- FORMAT (FIXES IF THERE IS UNDEFINED IN TABLE) --------------- */
+
+function formatCellData(cellData) {
+	if (cellData) {
+		return cellData;
+	} else {
+		return "-";
+	}
+}
+
